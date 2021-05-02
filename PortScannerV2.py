6b856cd8e164cd505bd1f91890d94ead0e340b11
Francisco <fgsj@cin.ufpe.br>
2021-05-01 23:35:56 -0300

    Second version port scanner incomplete

---
+++ PortScannerV2.py
@@ -0,0 +1,12 @@
+"""
+Creating second version of port scanner
+With code refactoring and searching up features
+Author = Francisco Junior
+"""
+
+#Importing libraries for network computer
+import sys
+import socket
+from datetime import datetime
+
+
