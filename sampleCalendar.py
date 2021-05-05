3fd71543659a93816772fa943b49f64c18fe5dae
Francisco <fgsj@cin.ufpe.br>
2021-05-05 00:22:43 -0300

    Creating and printing date now of calendar

---
+++ sampleCalendar.py
@@ -0,0 +1,12 @@
+"""
+Creating sample calendar with data now
+Author = Francisco Junior
+"""
+
+#Importing libraries
+import datetime
+
+
+#Creating variable with data now
+dataNow = datetime.datetime.now()
+
